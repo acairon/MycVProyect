4c6f4b3f7377adffc1159cabac8ed24664cd4747
Angel <angelshce@gmail.com>
2023-07-31 19:16:11 +0200

    Inicio shortenV2

--- Proyectos/acortador/app.py
+++ Proyectos/acortador/app.py
@@ -1,15 +1,15 @@
 import random
 import string
 import oracledb
-from fastapi import FastAPI, Request
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException, Form
+from fastapi.responses import HTMLResponse
+from starlette.staticfiles import StaticFiles
 
 app = FastAPI()
 
-# Conexión a la base de datos de Oracle
-def get_db_connection():
-    conn = oracledb.connect('usuario/oracle@nombre_de_servicio')
-    return conn
+# Montar el directorio "static" para servir el archivo index.html
+app.mount("/static", StaticFiles(directory="."), name="static")
+
 
 # Función para generar una cadena acortada aleatoria
 def generate_short_url():
@@ -17,41 +17,37 @@ def generate_short_url():
     short_url = ''.join(random.choice(characters) for _ in range(8))
     return short_url
 
-# Modelo para la solicitud de acortar URL
-class ShortenRequest(BaseModel):
-    url: str
-
-# Modelo para la respuesta de URL acortada
-class ShortenResponse(BaseModel):
-    shortened_url: str
 
 # Ruta para acortar una URL larga
-@app.post("/shorten", response_model=ShortenResponse)
-def shorten_url(request: ShortenRequest):
-    original_url = request.url
+@app.post("/shorten/", response_class=HTMLResponse)
+async def shorten_url(url: str = Form(...)):
+    if not url:
+        raise HTTPException(status_code=400, detail="URL no proporcionada")
 
     # Realizar la conexión a la base de datos de Oracle
-    with get_db_connection() as conn:
+    with oracledb.connect("usuario/oracle@nombre_de_servicio") as conn:
         # Crear un cursor para interactuar con la base de datos
         with conn.cursor() as cursor:
             # Verificar si la URL ya ha sido acortada previamente
-            cursor.execute("SELECT shortened_url FROM urls WHERE original_url = :original_url", {'original_url': original_url})
+            cursor.execute("SELECT shortened_url FROM urls WHERE original_url = :original_url", {'original_url': url})
             row = cursor.fetchone()
             if row:
                 short_url = row[0]
             else:
                 # Generar una URL acortada nueva
                 short_url = generate_short_url()
-                cursor.execute("INSERT INTO urls (original_url, shortened_url) VALUES (:original_url, :short_url)", {'original_url': original_url, 'short_url': short_url})
+                cursor.execute("INSERT INTO urls (original_url, shortened_url) VALUES (:original_url, :short_url)", {'original_url': url, 'short_url': short_url})
                 conn.commit()
 
-    return ShortenResponse(shortened_url=f'http://apishorten.angelcairon.com/{short_url}')
+    # Devolver el HTML con la URL acortada
+    return f'<html><body><h1>URL acortada: <a href="{short_url}" target="_blank">{short_url}</a></h1></body></html>'
+
 
 # Ruta para redireccionar a la URL original
-@app.get("/{short_url}")
-def redirect_to_original(short_url: str, request: Request):
+@app.get("/{short_url}", response_class=HTMLResponse)
+async def redirect_to_original(short_url: str):
     # Realizar la conexión a la base de datos de Oracle
-    with get_db_connection() as conn:
+    with oracledb.connect("usuario/oracle@nombre_de_servicio") as conn:
         # Crear un cursor para interactuar con la base de datos
         with conn.cursor() as cursor:
             # Buscar la URL original en la base de datos
@@ -59,6 +55,10 @@ def redirect_to_original(short_url: str, request: Request):
             row = cursor.fetchone()
             if row:
                 original_url = row[0]
-                return request.redirect(original_url)
+                if not original_url.startswith('http://') and not original_url.startswith('https://'):
+                    original_url = 'http://' + original_url  # Asegurarse de que la URL tenga el prefijo http:// o https://
+                conn.close()
+                return f'<html><body><h1>Redirigiendo a: <a href="{original_url}" target="_blank">{original_url}</a></h1></body></html>'
             else:
-                return "URL acortada no encontrada"
+                conn.close()
+                raise HTTPException(status_code=404, detail="URL acortada no encontrada")
