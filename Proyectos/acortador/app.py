01ea417d44be37835e47b4c1b11d6a9e0116f17a
Angel <angelshce@gmail.com>
2023-07-31 16:57:29 +0200

    cambios en la api

--- Proyectos/acortador/app.py
+++ Proyectos/acortador/app.py
@@ -1,11 +1,11 @@
-import os
 import random
 import string
 import oracledb
+import os
 from flask import Flask, redirect, request, jsonify
 from flask_cors import CORS
 
-from dotenv import load_dotenv
+app = Flask(__name__)
 
 load_dotenv()
 
@@ -17,9 +17,7 @@ un = os.environ.get('PYTHON_USERNAME')
 pw = os.environ.get('PYTHON_PASSWORD')
 cs = os.environ.get('PYTHON_CONNECTSTRING')
 
-
 # Función para generar una cadena acortada aleatoria
-
 def generate_short_url():
     characters = string.ascii_letters + string.digits
     short_url = ''.join(random.choice(characters) for _ in range(8))
@@ -39,29 +37,9 @@ def shorten_url():
         # Crear un cursor para interactuar con la base de datos
         with conn.cursor() as cursor:
             # Verificar si la URL ya ha sido acortada previamente
-            cursor.execute("SELECT shortened_url FROM urls WHERE original_url=:original_url", {'original_url': original_url})
+            cursor.execute("SELECT original_url FROM urls WHERE shortened_url = :short_url", {'short_url': f'http://apishorten.angelcairon.com/{original_url}'})
             row = cursor.fetchone()
 
-            if row:
-                short_url = row[0]
-            else:
-                # Generar una URL acortada nueva
-                short_url = generate_short_url()
-                cursor.execute("INSERT INTO urls (original_url, shortened_url) VALUES (:original_url, :short_url)", {'original_url': original_url, 'short_url': short_url})
-                conn.commit()
-
-    return jsonify({'shortened_url': f'http://apishorten.angelcairon.com/{short_url}'}), 200
-
-# Ruta para redireccionar a la URL original
-@app.route('/<short_url>')
-def redirect_to_original(short_url):
-    # Realizar la conexión a la base de datos de Oracle
-    with oracledb.connect(user=un, password=pw, dsn=cs) as conn:
-        # Crear un cursor para interactuar con la base de datos
-        with conn.cursor() as cursor:
-            # Buscar la URL original en la base de datos
-            cursor.execute("SELECT original_url FROM urls WHERE shortened_url=:short_url", {'short_url': short_url})
-            row = cursor.fetchone()
             if row:
                 original_url = row[0]
                 return redirect(original_url, code=302)
