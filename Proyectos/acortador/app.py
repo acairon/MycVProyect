0287e106f4ee163bd60c61fa21a88833d9ec9bf1
Angel <angelshce@gmail.com>
2023-07-31 19:06:23 +0200

    Inicio shortenV2

--- Proyectos/acortador/app.py
+++ Proyectos/acortador/app.py
@@ -1,17 +1,17 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template_string
 import hashlib
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template_string(open('templates/index.html').read())
 
 @app.route('/shorten', methods=['POST'])
 def shorten():
     long_url = request.form['long_url']
     short_url = hashlib.md5(long_url.encode()).hexdigest()[:8]
-    return render_template('index.html', short_url=short_url)
+    return render_template_string(open('templates/index.html').read(), short_url=short_url)
 
 if __name__ == '__main__':
     app.run(debug=True)
